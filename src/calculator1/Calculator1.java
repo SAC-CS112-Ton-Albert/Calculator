fab9f1834e7e799b124087899ab8e7787343a2ef
SAC-CS112-Ton-Albert <albertton@Alberts-MacBook-Pro.local>
2016-09-10 15:43:57 -0700

    Calculator
    Commit

---
+++ src/calculator1/Calculator1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author albertton
+ */
+public class Calculator1 {
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+    
+    int x;
+    int y;
+    
+    System.out.print("Enter first integer:");
+    x = input.nextInt();
+    
+    System.out.print("Enter second integer:");
+    y = input.nextInt();
+    
+    int sum = x + y; //sum
+    int difference = x - y; //difference
+    int product = x * y; //mutiplies
+    int quotient = x / y; //divides
+    
+    System.out.printf("Sum is %d\n", sum);
+    System.out.printf("Difference is %d\n", difference);
+    System.out.printf("Product is %d\n", product);
+    System.out.printf("Quotient is %d\n", quotient);
+    
+            
+    
+    
+            
+    
+        // TODO code application logic here
+    }
+    
+}
